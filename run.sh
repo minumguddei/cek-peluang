ad7827f7b53b37ce6f16b71ddbeaa73d4ae81894
minumguddei <minumguddei@gmail.com>
2026-04-07 09:14:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-# 1. Ambil data
+# 1. Ambil data blok terbaru
 DATA=$(curl -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}' $RPC_URL)
 
 # 2. Cek API
 [[ $DATA == *"result"* ]] && API=1 || API=0
 
-# 3. Parsing Gas & Bot
-GAS_HEX=$(echo $DATA | jq -r '.result.transactions[].gas')
-TXS=0; BOT=0
-for g in $GAS_HEX; do
-    TXS=$((TXS + 1))
-    # Konversi hex ke decimal dengan python agar aman dari overflow
-    d=$(python3 -c "print(int('$g', 16))")
-    [[ $d -gt 150000 ]] && BOT=$((BOT + 1))
-done
+# 3. Parsing Gas & Estimasi Bot (Pakai Python agar akurat)
+BOT_STATS=$(python3 -c "
+import json
+data = json.loads('''$DATA''')
+txs = data['result']['transactions']
+total = len(txs)
+# Filter Bot: Gas > 150k ATAU ada priority fee
+bots = [t for t in txs if int(t['gas'], 16) > 150000]
+print(f'{total} {len(bots)}')
+")
 
-# 4. Parsing Bribe (Pakai Python untuk presisi 18 desimal)
-B_LIST=$(echo $DATA | jq -r '.result.transactions[] | (.maxPriorityFeePerGas // .gasPrice // "0x0")')
+TXS=$(echo $BOT_STATS | cut -d' ' -f1)
+BOT=$(echo $BOT_STATS | cut -d' ' -f2)
 
-# Gunakan skrip Python singkat untuk hitung Max & Avg agar tidak 0.000000
+# 4. Parsing Bribe (Mencakup GasPrice & PriorityFee)
+# Kita ambil data bribe dari semua transaksi untuk melihat skala pasar
 STATS=$(python3 -c "
-bribes = [int(x, 16) for x in '$B_LIST'.split() if int(x, 16) > 1000000000]
+import json
+data = json.loads('''$DATA''')
+bribes = []
+for t in data['result']['transactions']:
+    # Ambil yang tertinggi antara priority fee atau gas price (dikurangi base fee estimasi)
+    p = int(t.get('maxPriorityFeePerGas', t.get('gasPrice', '0x0')), 16)
+    if p > 1000000000: bribes.append(p)
+
 if not bribes:
     print('0.0 0.0')
 else:
@@ -33,9 +42,10 @@ else:
 MAX_E=$(echo $STATS | cut -d' ' -f1)
 AVG_E=$(echo $STATS | cut -d' ' -f2)
 
-# 5. Compile & Lapor
+# 5. Eksekusi Audit C
 gcc bot.c -o bot
 ./bot $API $TXS $BOT $MAX_E $AVG_E > out.txt
 echo "$(date)" >> out.txt
 
-curl -H "Title: Gerbangku Audit" -d "$(<out.txt)" https://ntfy.sh/GERBANGKU
+# 6. Lapor NTFY
+curl -H "Title: Intelijen GERBANGKU" -d "$(<out.txt)" https://ntfy.sh/GERBANGKU
